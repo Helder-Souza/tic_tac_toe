4150c02198572546434532d666db2c022093336f
Helder-Souza <helder825@hotmail.com>
2025-06-05 09:24:14 -0300

    add `.gitignore` file and delete files

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.game.tictactoe {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.game.tictactoe to javafx.fxml;
-    exports com.game.tictactoe;
-}
